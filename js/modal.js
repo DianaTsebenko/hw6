ea0c52efa27bbc66dfe4949a576e133c8b591ebd
DianaTsebenko <cebenkodiana@gmail.com>
2024-03-07 21:48:17 +0200

    Finish hw 6

--- js/modal.js
+++ js/modal.js
@@ -7,6 +7,12 @@
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.modal.addEventListener("click", function(event){
+    if (
+      !event.target.closest("[modal-form]") ){
+        toggleModal()
+      }
+  })
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
